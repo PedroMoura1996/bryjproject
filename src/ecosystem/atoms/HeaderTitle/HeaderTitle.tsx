d712401c988a98776ebe31847303302e5c3f2de4
Pedro Moura <pedro.alexandre.moura1996@gmail.com>
2023-11-16 23:28:52 +0000

    adding logic to change the translations from en to fr and vice versa

--- src/ecosystem/atoms/HeaderTitle/HeaderTitle.tsx
+++ src/ecosystem/atoms/HeaderTitle/HeaderTitle.tsx
@@ -2,10 +2,22 @@ import Switch from "@mui/material/Switch";
 import { useTranslation } from "react-i18next";
 import "./headertitle.scss";
 import { useTheme } from "../../wrappers/ThemeProvider/ThemeProvider";
+import { useState } from "react";
 
 const HeaderTitle = () => {
-  const { t } = useTranslation();
+  const {
+    t,
+    i18n: { changeLanguage, language },
+  } = useTranslation();
   const { darkMode, toggleTheme } = useTheme();
+  const [lang, setLang] = useState(language);
+
+  const handleChangeHandler = () => {
+    const newLanguage = lang === "en" ? "fr" : "en";
+    changeLanguage(newLanguage);
+    setLang(newLanguage);
+  };
+
   return (
     <div className="header-title-container">
       <h1
@@ -23,6 +35,14 @@ const HeaderTitle = () => {
         <span id="switch-text">{`${t("darkMode")} ${
           darkMode ? t("on") : t("off")
         }`}</span>
+      </div>
+      <div className="translation-switch-container">
+        <Switch
+          id="change-language"
+          checked={lang === "en"}
+          onClick={handleChangeHandler}
+        />
+        <span id="switch-text-lang">{`${lang === "en" ? "EN" : "FR"}`}</span>
       </div>
     </div>
   );
